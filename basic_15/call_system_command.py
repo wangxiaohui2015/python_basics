973392bcfd9474fa2ff48bddf3510c8a305d9c9e
Sunny Wang <wxhyzh2005@163.com>
2021-05-21 07:53:06 +0800

    invoke command

--- basic_15/call_system_command.py
+++ basic_15/call_system_command.py
@@ -12,39 +12,41 @@ def is_linux():
 
 
 def execute(command, timeout=30):
-    # p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p = subprocess.Popen("dir C:", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #timer = Timer(timeout, p.terminate())
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("Run command: %r" % command)
     try:
-        #timer.start()
-        stdout, stderr = p.communicate(timeout=10)
+        stdout, stderr = p.communicate(timeout=timeout)
         ret_code = p.returncode
+        print("---------------")
+        print("Return code: %r" % ret_code)
+        print("Stdout: %r" % stdout)
+        print("Stderr: %r" % stderr)
+        print("---------------")
         return ret_code, stdout, stderr
     except Exception as e:
         raise e
-    finally:
-        #timer.cancel()
-        pass
+
+
+def import_backups(command, timeout=300, retry_times=5):
+    for i in range(retry_times):
+        print("Importing backups at #%r times..." % i)
+        try:
+            ret_code, stdout, stderr = execute(command, timeout)
+            if ret_code == 0:
+                print("Succeed to import backups.")
+                return True
+        except Exception as e:
+            print("Failed to import backups, exception: %r" % e)
+            print(e)
+    print("Failed to import backups.")
+    return False
 
 
 if __name__ == '__main__':
-    # subprocess.Popen("notepad.exe test.txt", shell = True)
-
-    #p = subprocess.Popen("dir C:", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #stdout, stderr = p.communicate()
-    #print(p.returncode)
-    #print(stdout)
-    #print(stderr)
-
-    #exit(0)
-
     ret = None
     if is_win():
-        ret = execute("notepad.exe")
+        ret = import_backups("python.exe D:\\work\\python_workspace\\python_study\\basic_15\\test_timeout.py", timeout=2)
     elif is_linux():
         ret = execute("ls /")
     else:
         print("Not supported platform.")
-    print(ret[0])
-    print(ret[1])
-    print(ret[2])
